5d82d9b804590d77baf38292090ca7d89982ffb5
anugerah prianka <anugerahprianka22@gmail.com>
2025-04-10 19:31:50 +0700

    [BAB 2] COMPONENT REACT NATIVE DAN TEXT HANDLING

---
+++ src/theme/colors.jsx
@@ -0,0 +1,18 @@
+const colors = {
+    pastelCream: (opacity = 1) => `rgba(255, 253, 208, ${opacity})`,
+    pastelPink: (opacity = 1) => `rgba(255, 182, 193, ${opacity})`,
+    pastelBlue: (opacity = 1) => `rgba(173, 216, 230, ${opacity})`,
+    pastelYellow: (opacity = 1) => `rgba(255, 239, 213, ${opacity})`,
+    pastelGreen: (opacity = 1) => `rgba(152, 251, 152, ${opacity})`,
+    pastelPurple: (opacity = 1) => `rgba(221, 160, 221, ${opacity})`,
+    pastelOrange: (opacity = 1) => `rgba(255, 218, 185, ${opacity})`,
+    grey: (opacity = 1) => `rgba(109, 125, 154, ${opacity})`,
+    blue: (opacity = 1) => `rgba(53, 88, 225, ${opacity})`,
+    white: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+    black: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    darkModeBlack: (opacity = 1) => `rgba(27, 27, 27, ${opacity})`,
+    darkModeBlue: (opacity = 1) => `rgba(146, 156, 241, ${opacity})`,
+    maroon: (opacity = 1) => `rgba(128, 0, 0, ${opacity})`, // Tambahkan 
+    gold: (opacity = 1) => `rgba(255, 215, 0, ${opacity})`, // Tambahkan 
+}
+export default colors;
